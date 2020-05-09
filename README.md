9c582df75542174fc7ff2d1684320158313d5bb5
lamartire <lamartire@gmail.com>
2020-05-09 17:41:17 +0300

    feat: initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sharec react app
+
+[![npm](https://img.shields.io/npm/v/sharec-essential-config)](https://npmjs.com/sharec-react-app)
+![MIT License](https://camo.githubusercontent.com/4481c7672053be9c676fbc983c040ca59fddfa19/68747470733a2f2f696d672e736869656c64732e696f2f6e706d2f6c2f6c6f6775782d70726f636573736f722e737667)
+
+This configuration includes demo application built with `react`.
---
+++ configs/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.jsx'),
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'static/index.html')
+  })],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
+}
